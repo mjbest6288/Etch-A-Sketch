84e78047403306064b7b75633c1b0aeb1576d4ed
Matthew Best <mjbest6288@gmail.com>
2024-06-04 15:13:38 -0400

    Added style and edited HTML and JS

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const container = document.querySelector("#container");
+const container = document.querySelector(".container");
+
+for(let i = 0; i < (16 * 16); i++){
+    const div = document.createElement("div");
+    container.appendChild(div);
+    div.setAttribute("style", "color: black; background: green; height: 16px; width: 16px");
+}
+
+
+//div.textContent = "this is some stuff";
+
+
+
+
+
+//div.setAttribute("style", "color: black; background: green; height: 16px; width: 16px");
+
+//div.classList.add("content");
